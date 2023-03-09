3eeb0df9d8885d0955a9d41ef5be00f6c58557ed
0r4cle <64685803+0r4cle@users.noreply.github.com>
2023-03-09 22:12:02 +0100

    Auto-generated commit [ci skip]

--- src/app/modules/landing/landing/landing.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { LandingComponent } from 'app/modules/landing/landing/landing.component';
-
-export const landingRoutes: Route[] = [
-    {
-        path: '',
-        component: LandingComponent,
-    },
-];
