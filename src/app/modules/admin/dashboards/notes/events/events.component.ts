628e2613b9a98de5ee4900fd8b0795d5d8450189
0r4cle <64685803+0r4cle@users.noreply.github.com>
2023-03-09 22:30:40 +0100

    'Init'

---
+++ src/app/modules/admin/dashboards/notes/events/events.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'events',
+    templateUrl: './events.component.html',
+    encapsulation: ViewEncapsulation.None,
+})
+export class EventsComponent {
+    constructor() {}
+}
---
+++ src/app/modules/admin/dashboards/notes/settings/settings.routing.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { SettingsComponent } from 'app/modules/admin/dashboards/notes/settings/settings.component';
+
+export const settingsRoutes: Route[] = [
+    {
+        path: '',
+        component: SettingsComponent,
+    },
+];
